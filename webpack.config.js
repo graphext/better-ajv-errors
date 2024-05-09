7bc209fa104a67a3941ee7ab3e3caaec1f3edcd6
Carlos Ruiz <carlos@graphext.com>
2024-05-09 17:01:53 +0200

    fix(webpack): add support for nodejs v20

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 var path = require('path');
+
+const crypto = require('crypto');
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm =>
+  crypto_orig_createHash(algorithm === 'md4' ? 'sha256' : algorithm);
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -7,5 +13,5 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     library: 'better-ajv-errors',
     libraryTarget: 'commonjs2',
-  }
+  },
 };
